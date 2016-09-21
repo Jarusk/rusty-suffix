579553182dfe9f2e4e0d55880c622679364a45db
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2016-09-21 15:21:51 -0400

    Added method to classify chars in input string

--- src/suffix_array.rs
+++ src/suffix_array.rs
@@ -1,14 +1,40 @@
 pub fn build_suffix_array(s: &str) -> Vec<usize> {
     let n = s.len() + 1;
-    let mut t = vec![true;n];
+    let mut stype = build_stype(s,n);
     let mut s1 = vec![0usize;n];
     let mut p1 = vec![0usize;n];
 
-    // Classify the type of each element in the list
-    for x in (0..n).rev() {
-        println!("{:?}",&x);
-    }
+    println!("{:?}",&stype);
     Vec::new()
+}
+
+/// Classify the string characters as either S or L.
+/// 'True' signifies the corresponding character is S type.
+pub fn build_stype(s: &str, size: usize) -> Vec<bool> {
+    //Handle the termination char
+    let mut cur_pos = size-1;
+    let mut item_1 = '$';
+    let mut item_1_stype = true;
+    let mut stype = vec![true;size];
+    stype[cur_pos] = true;
+    
+    // Classify the type of each element in the list
+    for item in s.chars().rev() {
+        cur_pos -= 1;
+        if item.lt(&item_1) {
+            item_1_stype = true;
+            stype[cur_pos] = true;
+        }else if item.gt(&item_1) {
+            item_1_stype = false;
+            stype[cur_pos] = false;
+        }else{
+            stype[cur_pos] = item_1_stype;
+        }
+
+        item_1 = item;
+    }
+
+    return stype;
 }
 
 #[cfg(test)]
@@ -19,5 +45,11 @@ mod tests {
     fn test_build_suffix_array() {
         let result = build_suffix_array("ACATTCGT");
         assert_eq!(1,1);
+    }
+
+    #[test]
+    fn test_build_stype() {
+        let expected = vec![true, false, true, false, false, true, true, false, true];
+        assert_eq!(expected, build_stype("ACATTCGT",9));
     }
 }
