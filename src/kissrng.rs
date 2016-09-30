ba55f470efbbf2318bc41578b8e7f36cada04a74
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2016-09-30 12:33:01 -0400

    Changed to xors for init and shifts

--- src/kissrng.rs
+++ src/kissrng.rs
@@ -11,20 +11,20 @@ impl KissRng {
     pub fn new() -> KissRng {
         let now = time::precise_time_ns() as usize;
         return KissRng {
-            x: 123456789usize.wrapping_add(now),
-            y: 987654321usize.wrapping_add(now),
-            z: 43219876usize.wrapping_add(now),
-            c: 6543217usize.wrapping_add(now)
+            x: 123456789usize ^ now,
+            y: 987654321usize ^ now,
+            z: 43219876usize ^now,
+            c: 6543217usize ^ now
         };
     }
 
     pub fn next(&mut self) -> usize {
         self.x = 314527869usize.wrapping_mul(self.x).wrapping_add(1234567);
-        self.y ^= self.y.rotate_left(5);
-        self.y ^= self.y.rotate_right(7);
-        self.y ^= self.y.rotate_left(22);
+        self.y ^= self.y.wrapping_shl(5);
+        self.y ^= self.y.wrapping_shr(7);
+        self.y ^= self.y.wrapping_shl(22);
         self.z = 4294584393usize.wrapping_mul(self.z).wrapping_add(self.c);
-        self.c = self.z.rotate_right(32);
+        self.c = self.z.wrapping_shr(32);
         return self.x.wrapping_add(self.y).wrapping_add(self.z);
     }
 }
